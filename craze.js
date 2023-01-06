02ee90b49ffee6ad8c4932b38990426ffd0e3b89
Minion <nessakristo@gmail.com>
2023-01-06 18:17:50 +0100

    do not display form on load

--- craze.js
+++ craze.js
@@ -1,3 +1,5 @@
+window.onload = document.getElementById("contact").style.display = "none";
+
 function myFunction() {
     var x = document.getElementById("contact");
     if (x.style.display === "none") {
